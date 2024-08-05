9237e06801563b0e6071348c0b72ddcc925b096d
Tatiannah <tatyral1704@gmail.com>
2024-08-05 03:13:23 +0300

    section contact à remplir

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 <h1 align="center"> ⚡️ DevFolio ⚡️</h1>
 
-[![Github](https://img.shields.io/github/license/anilseervi/DevFolio?color=%2312c2e9&logo=github&style=for-the-badge)](https://github.com/AnilSeervi/DevFolio/blob/master/README.md) ![Github Stars](https://img.shields.io/github/stars/anilseervi/DevFolio?color=%23c471ed&style=for-the-badge) ![Github forks](https://img.shields.io/github/forks/anilseervi/DevFolio?color=%23f64f59&style=for-the-badge) ![GitHub deployments](https://img.shields.io/github/deployments/anilseervi/DevFolio/github-pages?label=Deploy&logo=github&style=for-the-badge)
-
 ## A Modern Portfolio Template for Developers!
 
 <h2 align="center">
@@ -18,9 +16,6 @@
 ⚡️ Valid HTML5 & CSS3\
 ⚡ Fast and SEO Optimized
 
-To view a demo example, **[Click Here](https://devfolio.js.org)**\
-To view a live example, **[Click Here](https://anilseervi.github.io/Portfolio/)**\
-To view live example repo, **[Click Here](https://github.com/AnilSeervi/Portfolio/)**
 
 ## Getting Started 🚀
 
@@ -303,9 +298,7 @@ $secondary-color: #5b86e5;
 - [ScrollReveal.js](https://scrollrevealjs.org/) - JavaScript library
 - [Tilt.js](https://gijsroge.github.io/tilt.js/) - JavaScript tiny parallax library
 
-## Authors
 
-- **Anil Seervi** - [https://github.com/AnilSeervi](https://github.com/AnilSeervi)
 
 ## License 📄
 
--- javascript/tabs.js
+++ javascript/tabs.js
@@ -1,24 +1,23 @@
-function openTab(evt, tabName) {
-    // Cache tout le contenu des onglets
-    var i, tabContent, tabButtons;
+var setPostHeights = function() {
+    var maxHeight = -1,
+        post = $('.posts .post');
 
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
+    $(post).height('auto');
 
-    // Enlève la classe "active" de tous les boutons
-    tabButtons = document.getElementsByClassName("tab-button");
-    for (i = 0; i < tabButtons.length; i++) {
-        tabButtons[i].className = tabButtons[i].className.replace(" active", "");
-    }
-
-    // Affiche le contenu de l'onglet sélectionné et ajoute la classe "active" au bouton
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    $(post).each(function() {
+        maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+    });
+    $(post).height(maxHeight);
 }
 
-// Affiche le premier onglet par défaut
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".tab-button").click();
+$(document).ready(function () {
+    setPostHeights();
+});
+
+var timer;
+$(window).bind('resize', function() {
+    clearTimeout(timer);
+    timer = setTimeout(function(){
+        setPostHeights();
+    }, 100);
 });
